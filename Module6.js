a61b914d34344151b0e3e30d05825309c959f70a
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:10:30 +0530

    Create Module6.js

---
+++ Module6.js
@@ -0,0 +1,119 @@
+AWS Cloud Foundation Module 6 Answers
+Q1. Why is AWS more economical than traditional data centers for applications with varying 
+       compute workloads? (Select the best answer).
+
+A. Amazon EC2 Costs are billed on a monthly basis.
+B. Customers retain full administrative access to their Amazon EC2 instances.
+C. Customers can permanently run enough instances to handle peak workloads.
+D. Amazon EC2 instances can be launched on-demand when needed.
+
+Answer: 
+                [ D ] Amazon EC2 instances can be launched on-demand when needed.
+
+Q2. If your project requires monthly reports that iterate through very large amounts of data, 
+       which Amazon Elastic Compute Cloud EC2 purchasing option should you consider? 
+       (Select the best answer).
+
+A. Spot Instances.
+B. Scheduled Reserved Instances.
+C. Dedicated Hosts.
+D. On-Demand Instances.
+
+Answer: 
+                [ B ] Scheduled Reserved Instances.
+
+Q3. What is included in an Amazon Machine Image (AMI)? Select the best answer.
+
+A. A template for the root volume for the instance.
+B. Launch permissions that control which AWS accounts can use the AMI to launch instances.
+C. A block device mapping that specifies the volumes to attach to the instance when it's launched.
+D. All of the above.
+
+Answer: 
+                  [ D ] All of the above.
+
+
+Q4. Which Amazon Elastic Compute Cloud EC2 feature ensures your instances will not share 
+        a physical host with instances from any other AWS customer? (Select the best answer).
+
+A. Amazon VPC.
+B. Placement Groups.
+C. Dedicated Instances.
+D. Reserved Instances.
+
+Answer: 
+                  [ C ]  Dedicated Instances.
+
+Q5. Which of the following services is a serverless compute service in AWS? (Select the best answer).
+
+A. AWS Config.
+B. AWS Lambda.
+C. AWS OpsWork.
+D. Amazon EC2.
+
+Answer: 
+                     [ B ]  AWS Lambda.
+
+Q6. What is the service provided by AWS that enables developers to easily deploy and 
+       manage applications in the cloud? (Select the best answer).
+
+A. Amazon Elastic Container Service.
+B. AWS Elastic Beanstalk.
+C. AWS OpsWork
+D. AWS CloudFormation
+
+Answer: 
+                   [ B ]  AWS Elastic Beanstalk.
+
+*Elastic Beanstalk is an AWS compute service option. It is a Platform as a Service (or PaaS) that facilitates quick deployment, scaling, and managing of your web applications and services.
+
+Q7. Your web application needs four instances to support steady traffic all of the time. 
+      On the last day of the month, the traffic triples. What is the most cost-effective way to 
+      handle this pattern?  (Select the best answer).
+
+A. Run 12 reserved instances all of the time.
+B. Run four On-demand Instances constantly, then add eight more On-Demand on the last day of 
+     each month.
+C. Run four Reserved Instances constantly, then add eight On-Demand Instances on the last day of 
+     each month.
+D. Run four On-Demand Instances constantly, then add eight Reserved Instances on the last day of 
+     each month.
+
+Answer: 
+Run four Reserved Instances constantly, then add eight On-Demand Instances on the last day of each month.
+
+*Reserved Instances provide cost savings when you can commit to running instances full-time, such as to handle the base traffic. On-Demand Instances provide the flexibility to handle traffic spikes.
+
+
+Q8. True or false? Containers contain an entire operating system.
+
+True.
+False.
+
+Answer: 
+                  False.
+
+*Containers are smaller than virtual machines, and do not contain an entire operating system.
+
+Q9. Which Amazon EC2 option is best for long-term workload with predictable usage patterns? 
+       (Select the best answer).
+
+A. Spot Instances.
+B. On-Demand Instances.
+C. Reserved Instances.
+
+Answer: 
+                    [ C ] Reserved Instances.
+
+Q10. Which of the following must be specified when launching a new Amazon Elastic Cloud 
+
+         Compute EC2 windows instance? (Choose two).
+
+A. Amazon EC2 instance type.
+B. Amazon Machine Image (AMI).
+C. The Amazon EC2 Instance ID.
+D. Password for the administrator account.
+E. Amazon EC2 instance type.
+
+Answer: 
+                     [ B ]  Amazon Machine Image (AMI).
