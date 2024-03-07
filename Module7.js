05ffec013824b66c37a063cac3708a6c326ebb78
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:11:52 +0530

    Create Module7.js

---
+++ Module7.js
@@ -0,0 +1,125 @@
+AWS Academy Cloud Foundation Module 7 Quiz Answers
+
+Q1. True or false. Amazon simple storage service S3 is an object storage suitable for the storage    of flat files like Microsoft Word documents, photos, etc.
+
+
+A. True
+
+B. False
+
+Answer:   True.
+
+Q2. Amazon S3 replicates all objects _____. (Select the best answer).
+
+A. On multiple volumes within an availability zone.
+
+B. In multiple Availability zones within the same region.
+
+C. Across multiple regions for higher durability.
+
+D. On multiple S3 buckets.
+
+Answer:  In multiple Availability zones within the same region.
+
+Q3. Which of the following can be used as a storage class for an S3 object lifecycle policy?
+
+A. S3 Standard Access.
+
+B. AWS Storage Gateway.
+
+C. S3 Infrequent Access.
+
+D. Simple Storage Service Glacier.
+
+E. S3 Reduced Redundancy Storage.
+
+F. Amazon Dynamo DB.
+
+Answer:
+               [ D ] , [ C ] , [ A ]  Glacier, S3 infrequent access and S3 standard access.
+
+Q4. The name of an S3 bucket must be unique.
+
+A. Worldwide across all AWS accounts.
+
+B. Within a region.
+
+C. Across all your AWS accounts.
+
+D. Within your AWS account.
+
+Answer: [ A ] Worldwide across all AWS accounts.
+
+Q5. You can use amazon elastic file system efc to: (Select the best answer).
+
+A. Provide simple, scalable, elastic file storage for use only within AWS.
+
+B. Implement storage for Amazon EC2 instances that multiple virtual machines can access at the 
+
+     same time.
+
+C. Host a robust CDN to deliver entire web sites with dynamic, static, and streaming content.
+
+D. Generate user specific content.
+
+Answer: [ B ] 
+
+Implement storage for EC2 instances that multiple virtual machines can access at the same time.
+
+Q6. Amazon Elastic Block Storage (Amazon EBS) is recommended when 
+
+       data _____ and ______ (Choose two).
+
+ A. Requires object level storage.
+
+B. Must be quickly accessbile, requiring long-term persistence.
+
+C. Requires an encryption solution.
+
+D. Needs to be stored in a different availability zone than the one the EC2 instance is in.
+
+
+Answer: [ B ] , [ C ] 
+
+Must be quickly accessible, requiring longterm persistence
+requires an encryption solution.
+Q7. True or false, by default all data stored in amazon S3 is viewable by the public.
+
+A. True.
+
+B. False.
+
+Answer: [ B ] 
+                          False.
+
+Q8. Regarding amazon S3 glacier, what is a vault?
+
+A. The rules that determine who may (or may not) access archives.
+
+B. An object (photos, videos, files, or documents).
+
+C. A container for storing archives.
+
+D. A policy that identifies who can access content stored in Glacier.
+
+Answer: [ C]  Container for storing archives.
+
+Q9. True or false. When you create a bucket in amazon S3, it is associated with a specific aws region.
+
+A. True.
+
+B. False.
+
+Answer: [ A]  True.
+
+Q10. Which of the following are features of Amazon Elastic Block Store (Amazon EBS). (Choose two).
+
+A. Data on an Amazon EBS volume is lost when the attatched instance is stopped.
+
+B. Amazon EBS volumes can be encrypted transparently to workloads on the attached instance.
+
+C. Amazon EBS data is automatically backed up to tape.
+
+D. Data stored on Amazon  EBS is automatically replicated within an Availability Zone.
+
+Answer: [ B ] , [ D]
