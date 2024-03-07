50c6d2f3dfedb869048a4462a6a67436106827de
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:07:20 +0530

    Create Module3.js

---
+++ Module3.js
@@ -0,0 +1,138 @@
+Home
+Aws
+AWS Academy Cloud Foundation Module 3 Quiz Answers | AWS Global Infrastructure Overview
+by
+study2night
+-
+May 18, 2023
+0
+In this article we will discuss about the AWS Global Infrastructure Overview Answers for the Cloud Foundation Module Course. These Cloud Foundation Course helps in finding out the skills in the AWS cloud. 
+
+Read More:  AWS Cloud Foundation Module 2 Answers  
+
+AWS Global Infrastructure Overview answers
+
+
+AWS Global Infrastructure Overview
+Q1. Which component of the AWS Global Infrastructure does Amazon CloudFront use to ensure 
+
+       low-latency delivery? (Select the best answer)
+
+ A. AWS Regions
+
+ B. AWS Edge locations
+
+ C. AWS Availability Zone
+
+D. Amazon virtual private cloud (Amazon VPC)
+
+Answer:  
+
+                  [ B ] AWS Edge locations
+
+Q2. You can run applications and workloads from a region closer to the end users to ______ latency.
+
+A. Increase
+
+B. Decrease
+
+Answer: 
+
+                  [ B ] Decrease
+
+Q3. True / False? Networking, storage, compute and databases are examples of service categories 
+
+       that AWS offers.
+
+Answer: 
+
+                 True
+
+Q4. Which of these statements about Availability Zones is not true? (Select the best answer)
+
+A. Availability Zones are designed for fault isolation
+
+B. Availability Zones are made up of one or more data centers
+
+C. Availability zones are connected to each other using high-speed private links
+
+D. A data center can be used for more than one availability zone
+
+Answer: 
+
+                 [ D ]   A data center can be used for more than one availability zone
+
+Q5. What is true about Regions (Choose two)
+
+1. They are the physical locations of your customers
+
+2. All regions are located in one specific geographic area
+3. A region is a physical location that has multiple availability zones
+
+4. Physical location has multiple availability zones
+
+5. Each region is located in a separate geographic area
+
+Answer: 
+
+3. A region is a physical location that has multiple availability zones
+
+5. Each region is located in a separate geographic area
+
+Q6. Which of the following are geographic areas that host two or more Availability Zones? 
+
+       (Select the best answer)
+
+A. AWS Origin
+
+B. AWS Regions
+
+
+C. Compute Zones
+
+D. Edge Locations
+
+Answer: AWS Regions
+
+Q7. _____ means the infrastructure has built-in component redundancy and _____ means that 
+
+      resources dynamically adjust to increases or decreases in capacity requirements.
+
+A. No human intervention, fault-tolerant
+
+
+B. Fault-tolerant, no human intervention
+
+C. Elastic and scalable, fault-tolerant
+
+D. Elastic and scalable, no human intervention
+
+E. Fault-tolerant, elastic and scalable
+
+Answer: [ E ] Fault-tolerant, elastic and scalable
+
+Q8. True or False? Availability Zones within a region are connected through low-latency links.
+
+Answer: 
+
+               True
+
+Q9. AWS highly recommends provisioning your compute resources across _____ availability zones.
+
+A.  All
+
+B. No
+
+C. Multiple
+
+D. Single
+
+Answer:
+
+                Multiple
+
+Q10. True or False? Edge locations are only located in the same general area as regions.
+
+Answer: 
+
+                False
