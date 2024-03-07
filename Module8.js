b3b470a7027e387bee72be090324afbcb82a9572
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:16:14 +0530

    Create Module8.js

---
+++ Module8.js
@@ -0,0 +1,145 @@
+Q1. You are designing an ecommerce web application that will scale to hundreds of thousands 
+
+       of concurrent users, Which database technology is best suited to hold the session state in 
+
+       this example? 
+
+A. Amazon Relational Database Service (Amazon RDS)
+
+B. Amazon DynamoDB 
+
+C. Amazon RedShift 
+
+D. Amazon Simple Storage (Amazon S3)
+
+
+Answer: 
+                  [ B ]  Amazon DynamoDB
+
+Q2. You need to find an item in Amazon DynamoDB table using an attribute other than item's
+
+        primary key. Which of the following operations should you use?
+
+A. Put Item
+
+B. Scan 
+
+C. Query
+
+D. Get Item 
+
+Answer:     [ B ] Scan
+
+
+Q3. In Amazon DynamoDB, what does the query operation enable you to do?
+
+A. Query a table using the Partition Key and optional sort key filter
+
+B. Query and secondary indexes that exists for a table 
+
+C. Efficiency retrieve items from a table or secondary index 
+
+D. All the Above    
+
+Answer:   [ D ] All of the above
+
+Q4. Which AWS Cloud service is best suited for analyzing your data by using 
+
+
+      standard structured  query language (SQL) and your existing business intelligence (BI) tools
+
+
+A. Amazon Relational Database (Amazon RDS)
+
+B. Amazon Simple Storage Simple Glacier 
+
+C. Amazon Dynamo DB
+
+D. Amazon Redshift
+
+Answer:  [ D ]  Amazon Redshift
+
+Q5. In Amazon DynamoDB, an attribute is________. 
+
+A. A Fundamental Data Element
+
+
+B. A Collection Of items 
+
+C. A Collection of Attributes
+
+Answer: [ A ] a fundamental data element
+
+Q6. Which of the following use cases is appropriate for using Amazon Relational Database 
+
+       Service (Amazon RDS)?
+
+A. Massive read / write rates
+
+B. Simple Gets or Put Requests
+
+C. Complex Transactions
+
+D. All the Above 
+
+Answer: [ C ] Complex transactions
+
+Q7. A company has an application, which consists of a .NET layer that connects to a 
+
+     MySQL database. They want to move this application on to AWS and use AWS features 
+
+     such as high availability and automated backups. Which of the following would be 
+
+     an ideal database for this use case ( Select the best answer)
+
+A. Amazon Dynamo DB 
+
+B. Amazon RDS 
+
+C. Amazon Aurora
+
+D. Amazon Redshift
+
+Answer: [ C ] Amazon Aurora
+
+Q8. If you are developing an application that requires a database with extremely 
+
+       fast performance, fast scalability, and flexibility in the database schema, which service 
+
+       would you consider?
+
+A. Amazon Relational Database (Amazon RDS)
+
+B. Amazon Elastic Cache 
+
+C. Amazon Dynamo DB
+
+D. Amazon Redshift
+
+Answer: [ C]  Amazon DynamoDB
+
+Q9. True of false? Amazon RDS automatically patches the database software and backs up 
+
+       your database, storing the backups for a user-defined retention period and enabling point-in-
+
+        time recovery
+
+A. True 
+
+B. False
+
+Answer: [ A ] True
+
+Q10. What should you consider when choosing database type? 
+
+A. Data Size 
+
+B. Data access period 
+
+C. Query Frequency
+
+D. All the Above 
+
+E. Highly Available 
+
+Answer:  [ D ] All of the above
