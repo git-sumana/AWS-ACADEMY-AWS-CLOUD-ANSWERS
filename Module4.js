5cc74d79fd8feca728fbb46e7cda25cbf190bd71
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:09:00 +0530

    Create Module4.js

---
+++ Module4.js
@@ -0,0 +1,149 @@
+AWS Cloud Security 
+
+Q1. What is the AWS Shared Security Model?
+
+Answer: Security of the Cloud
+
+Q2. In the shared responsibility model, which of the following are examples of
+       " Security in the cloud" ? 
+
+A. Compliance with compute security standards and regulations. 
+
+B. Physical security of the facilities in which the service operates. 
+
+C. Security group configurations. 
+
+D. Encryption of the data at rest and data in transit. 
+
+E. Protecting the global infrastructure. 
+
+
+Answer: 
+
+ [ C ] , [ D ]  
+
+ Q3. Which of the following is the responsibility of AWS under the AWS shared responsibility 
+        model? (Select the best answer).
+
+A. Configuring third party applications.
+
+B. Maintaining physical hardware.
+
+C. Security application access and data.
+
+D. Managing custom Amazon machine images (AMI's).
+
+Answer: 
+                Maintaining physical hardware.
+
+Q4. When creating an AWS Identity and Access Management (IAM) policy, what are the two 
+
+
+      types of access that can be granted to a user? (Choose two).
+
+A. Institutuional Access.
+
+B. Authorized Access.
+
+C. Programmatic Access.
+
+D. AWS Management Console Access.
+
+E. Administrative Root Access.
+
+Answer: 
+
+Programmatic Access.
+
+AWS Management Console Access.
+
+Q5. True or false? AWS organizations enables you to consolidate multiple aws accounts so that 
+
+       you centrally manage them.
+
+           True.
+
+           False.
+
+Answer: 
+                   True.
+
+Q6. Which of the following are best practices to secure your account using aws identity and 
+
+         access management? (Choose 2).
+
+A. Provide users with default administrative privileges.
+
+B. Leave unused and unnecessary users and credentials in place.
+
+C. Manage access to AWS resources.
+
+D. Avoid using IAM groups to grant the same access permissions to multiple users.
+
+E. Define fine-grained access rights.
+
+Answer: 
+
+Manage access to AWS resources.
+
+Define fine-grained access rights.
+
+Q7. Which of the following should be done by the AWS account root user? 
+
+       (Select the best answer).
+
+A. Secure access for applications.
+
+B. Integrate with other AWS services.
+
+C. Change granular permissions.
+
+D. Change the AWS support plan.
+
+Answer: 
+
+Change the AWS support plan.
+
+Q8. After initial login, what does AWS recommend as the best practice for the AWS account 
+
+       root user?
+
+A. Delete the AWS account root user.
+
+B. Revoke all permissions on the AWS account root user.
+
+C. Restrict permission on the AWS account root user.
+
+D. Delete the access keys of the AWS account root user.
+
+Answer: 
+
+Delete the access keys of the AWS account root user.
+
+Q9. How would a system administrator add an additional layer of login security to a user's 
+
+       AWS management console?
+
+A. Use Amazon Cloud Directory.
+
+B. Audit AWS Identity and Access Management (IAM) roles.
+
+C. Enable multi-factor authentication.
+
+D. Enable AWS CloudTrail.
+
+Answer: 
+
+Enable multi-factor authentication.
+
+Q10. True or false? AWS key management service (AWS KMS) enables you to assess, audit, 
+
+         and evaluate the configurations of your AWS resources.
+
+             True.
+
+             False.
+
+Answer: 
+
+                    False.
