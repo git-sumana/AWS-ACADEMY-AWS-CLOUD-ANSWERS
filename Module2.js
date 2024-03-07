0c6f7a8b50b8b6d83b8718a776787483237a8067
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:05:48 +0530

    Update Module2.js

--- Module2.js
+++ Module2.js
@@ -1,1 +1,71 @@
+Q1. For certain services like Amazon Elastic Compute Cloud (Amazon EC2) and 
 
+       Amazon Relational Database Service (Amazon RDS), you can invest in reserved capacity. 
+       What options are available for Reserved Instances?
+
+Answer: 
+              AURI
+              NURI
+              PURI
+
+Q2. Where can a customer go to get more details about Amazon Elastic Compute Cloud 
+       (Amazon EC2) billing activity that took the place 3 months ago?
+
+Answer: 
+
+               AWS Cost Explorer
+
+Q3. True or false? To receive the discounted rate associated with Reserved Instances, you must 
+        make a full, upfront payment for the term of the agreement
+
+Answer: 
+             False
+
+Q4. There is no charge for which of the following?
+
+Answer: 
+              Inbound data transfer (with some exceptions)
+              Data transfer between services within the same AWS Reg
+
+Q5. What are the four support plans offered by AWS Support?
+
+Answer: 
+              Basic, Developer, Business, Enterprise
+
+
+Q6. What AWS tool compares the cost of running your application in an on-premises data 
+       center to AWS?
+
+Answer: 
+             Total Cost of Ownership (TCO) calculator
+
+
+Q7. As AWS grows, the cost of doing business is reduced and savings are passed back to the 
+      customer with lower pricing. What is this optimization called?
+
+Answer: 
+              Economics of scale
+
+
+Q8. True or false? AWS offers a variety of services at no charge, for example, Amazon Virtual Private Cloud (Amazon VPC), AWS Identity and Access Management (IAM), 
+       Consolidated Billing, AWS Elastic Beanstalk, automatic scaling, AWS Ops Works and 
+       AWS CloudFormation. However, you might be charged for other AWS services that you use 
+       in conjunction with these services
+
+Answer: 
+                True
+
+
+Q9. When are free data transfers applicable across AWS?
+
+Answer: 
+
+               Free inbound data transfer for Amazon Elastic Computer Cloud (Amazon EC2) instances
+
+               Free outbound data transfer between AWS services within the same Region
+
+
+Q10. True of false? Unlimited services are available with the AWS Free Tier to new AWS customers for 12 months following their AWS sign-up date.
+
+Answer: 
+               False
