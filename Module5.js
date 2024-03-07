f5a2a7bc17ff7508c1b54afaf9b2fed49224b7d5
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:09:38 +0530

    Create Module5.js

---
+++ Module5.js
@@ -0,0 +1,140 @@
+Home
+Aws
+AWS Academy Cloud Foundation Module 5 Quiz Answers
+by
+study2night
+-
+May 20, 2023
+0
+Hey Folks, In this article we are going to see about the AWS Academy Cloud Foundation answers of Module 5. This Module 5 Contains about the networking and content delivery which plays a pivotal role in enabling seamless communication and access to online resources.
+
+Whether you're browsing the web, streaming your favorite shows, or engaging with cloud services, understanding the fundamental concepts behind networking and content delivery is essential. In this blog post, we'll embark on a journey to explore the foundations of these technologies, unravel their intricacies, and highlight their significance in our digital landscape.
+
+Read More:  AWS Cloud Foundation Module 4 Answers 
+
+AWS Academy Cloud Foundation Module 5 Quiz Answers
+
+
+AWS Academy Cloud Foundation Module 5 Quiz Answers 
+Q1. Which AWS networking service enables a company to create a virtual network within AWS
+
+A. AWS Config
+B. Amazon Route 53
+C. AWS Direct Connect
+D. Amazon VPC
+
+Answer: 
+
+               D. Amazon VPC
+
+Q2. With Amazon Virtual Private Cloud (Amazon VPC), what is the smallest size subnet you can 
+       have in a VPC?
+
+A. /28
+B. /30
+C. /24
+D. /26
+
+Answer: 
+                 A. /28
+
+Q3. With Amazon Virtual Private Cloud (Amazon VPC), what is the maximum size IP address 
+       range you can have in a VPC?
+
+A. /28
+B. /16
+C. /30
+D. /24
+
+Answer: 
+                B. /16
+
+
+Q4. You need to allow resources in a private subnet to access the internet. Which of the following 
+       must be present to enable this access?
+
+A. Network access control lists
+B. Security groups
+C. NAT gateway
+D. Route tables
+
+Answer: 
+  
+              C. NAT gateway
+
+
+Q5. Which AWS networking service enables a company to create a virtual network within AWS?
+
+A. Amazon Virtual Private Cloud (Amazon VPC)
+B. AWS Direct Connect
+C. Amazon Route 53
+D. AWS Config
+
+Answer: 
+               A. Amazon VPC
+
+
+Q7. T/F, Private subnets have direct access to the internet.
+
+Answer: 
+              False
+
+
+Q8. Which component of AWS Global Infrastructure does Amazon CloudFront use to ensure 
+       low-latency deliver?
+
+A. AWS Regions
+B. AWS Availability Zones
+C. AWS edge locations
+D. Amazon Virtual Private Cloud (Amazon VPC)
+
+Answer: 
+              C. AWS edge locations
+
+
+Q9. Which of the following is an optional security control that can be applied at the subnet layer 
+       of a VPC?
+
+A. Network ACL
+B. Security group
+C. Web application firewall
+D. Firewall
+
+Answer: 
+                A. Network ACL
+
+
+Q10. What happens when you use Amazon Virtual Private Cloud (Amazon VPC) to create a 
+         new VPC?
+
+A. Three subnets are created by default in one Availability Zone
+B. A main route table is created by default
+C. Three subnets are created by default: one for each Availability Zone
+D. An internet gateway is created by default.
+
+Answer: 
+               B. A main route table is created by default
+
+Q11. Which of the following can be used to protect Amazon Elastic Compute Cloud 
+         (Amazon EC2)  instances hosted in AWS?
+
+A. Internet Gateway
+B. All of the above
+C. AMI
+D. Security group
+
+Answer:
+              D. Security group
+
+Q12. You are a solutions architect who works at a large retail company that is migrating its 
+         existing infrastructure to AWS. You recommend that they use a custom VPC. When 
+         you create a VPC, you assign it to an IPv4 Classless Inter-Domain Routing (CIDR) block 
+         of 10.0.1.0/24 (which has 256 total IP addresses). How many IP addresses are available?
+
+A. 251
+B. 256
+C. 246
+D. 250
+
+Answer: 
+               [A]  251
