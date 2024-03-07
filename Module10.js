3657e6656fe78f48fa08413555721890f976dfa6
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:20:24 +0530

    Create Module10.js

---
+++ Module10.js
@@ -0,0 +1,158 @@
+AWS Academy Cloud Foundation Module 10 Quiz Answers
+
+
+AWS Academy Cloud Foundation Module 10 Quiz Answers
+Q1. Which of the following AWS tools help your application scale up or down based on demand?
+
+A. Availability Zones 
+
+B. Amazon EC2 Auto Scaling 
+
+C. Aws CloudFormation 
+
+D. Elastic load balancing 
+
+E. AWS Config 
+
+Answer: [ B ] [ D ]
+                                       Amazon EC2 Auto Scaling
+
+                                       Elastic Load Balancing
+
+Q2. Which service would you use to send alerts based on Amazon CloudWatch alarms?
+
+A. Amazon Simple Notification Service (Amazon SNS)
+
+B. AWS Cloud Trail 
+
+C. AWS Trusted Advisor 
+
+D. Amazon Route 53
+
+Answer:   [ A ]  Amazon Simple Notification Service (Amazon SNS)
+
+Q3. Which of the following are characteristics of Amazon EC2 Auto Scaling? ( Choose three ) 
+
+A. Only Supports Dynamic Scaling 
+
+B. Responds to changing conditions by adding or terminating instances
+
+C. Deliver Push Notifications 
+
+D. Launches instances from a specified Amazon Machine Image (AMI)
+
+E. Enforces a minimum number of running Amazon EC2 instances
+
+Answer: [ B ] , [ D ] , [ E ] 
+
+Responds to changing conditions by adding or terminating instances
+
+Launches instances from a specified Amazon Machine Image (AMI)
+
+Enforces a minimum number of running Amazon EC2 instances
+
+Q4. Which of the following must be configured on an Elastic Load Balancing load balancer 
+
+        to expect incoming traffic ?
+
+A. A Port 
+
+B. A Network Interface 
+
+C. A Listener 
+
+D. An Instance 
+
+Answer: [ C ] A listener
+
+Q5. Which of the following elements are used to create an Amazon EC2 Auto Scaling 
+
+       launch configuration?
+
+A. Amazon Machine Image (AMI)
+
+B. Load Balancer 
+
+C. Instance type
+
+
+D. Virtual Private Cloud (VPC ) and Subnets
+
+ E. Amazon Elastic Block Store (Amazon EBS) volumes
+
+Answer: [ A ] , [ C ] , [ E ] 
+
+Amazon Machine Image (AMI)
+
+Instance type
+
+Amazon Elastic Block Store (Amazon EBS) volumes
+
+
+Q6.Which of the following services can help you conflict important metrics from 
+
+      Amazon Relational Database Service (Amazon RDS) and Amazon Elastic Compute 
+
+      Cloud (Amazon EC2) instances?
+
+A. Amazon CloudFront 
+
+B. Amazon Cloud Search 
+
+C. Amazon Cloud Watch 
+
+D. Amazon Cloud Trial 
+
+E. Amazon E2C Scaling 
+
+Answer: [ D ] Amazon CloudTrail
+
+Q7. Which of the following are elements of an Auto Scaling group ? (Choose three)
+
+A. Maximum size 
+
+B. Health Checks 
+
+C. Desired capacity
+
+D. Minimum size
+
+Answer: [ A ] , [ C ] , [ D ]
+
+Maximum size
+
+Desired capacity
+
+Minimum size
+
+Q8. There is an adult at your company and they need to have a big log of all access to 
+
+        AWS resources in the account. Which of the following services can assist in providing 
+
+        these details?
+
+A. Amazon Cloud Watch 
+
+B. AWS Cloud Trial 
+
+C. Amazon Elastic Compute Cloud (Amazon EC2)
+
+D. Amazon Simple Notification Service ( Amazon SNS) 
+
+Answer: [ B ]  AWS CloudTrail
+
+Q9. In Elastic Load Balancing, when the load balancing detects an unhealthy target, which of 
+
+       the following are true (Choose three)
+
+A. Stops routing traffic to that target
+
+B. Triggers an Alarm 
+
+C. Resumes routing traffic when it detects that the target is healthy again
+
+D. Resumes routing traffic when manually restarted 
+
+E. Routes traffic to a healthy target
+
+Answer: [ A ] , [ C ] , [ E ]  
