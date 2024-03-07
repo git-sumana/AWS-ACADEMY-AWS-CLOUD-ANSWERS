42adfa4773e7d17bf5aeb4e08a507d55bd3ae6eb
Sumana Datta Kapavarapu <137371757+git-sumana@users.noreply.github.com>
2024-03-07 13:19:23 +0530

    Create Module9.js

---
+++ Module9.js
@@ -0,0 +1,144 @@
+AWS Academy Cloud Foundation Module 9 Quiz Answers
+
+Q1. Which of the following is not one of the four areas of the Performance Efficiency pillar of the 
+
+        AWS Well-Architected Framework?
+
+   A. Tradeoffs 
+
+   B. Selection 
+
+   C. Traceability 
+
+   D. Monitoring 
+
+   Answer:  [ C ]  Traceability
+
+Q2. Which of the following is a principle when designing cloud-based systems?
+
+   A. Build tightly - coupled components 
+
+   B. Make infrequent , large batch changes
+
+   C. Assume Everything will fail 
+
+   D. Use as many services as possible
+
+  Answer:  [ C ]  Assume everything will fail
+
+Q3. Which of the following are pillars of the AWS Well-Architected Framework? (Choose three)
+
+   A. Security 
+
+   B. Persistence 
+
+   C. Operational Excellence 
+
+   D. Cost Optimization  
+
+Answer: [ A ], [ C ] , [ D ]  Security, Operational Excellence & Cost Optimization  
+
+Q4. Which design principles are recommended when considering performance efficiency? (Choose 2)
+
+   A. Enable traceability 
+
+   B. Use serverless architecture 
+
+   C. Analyze and attribute expenditure 
+
+   D. Match Supply with demand 
+
+   E. Democratize advance technologies  
+
+Answer: [ B ] [ E ] 
+
+                                 Use serverless architecture
+
+                                 Democratize advanced technologies
+
+Q5. AWS Trusted Advisor provides insight regarding which five categories of an AWS account?
+
+A. Performance, cost optimization, security, fault tolerance, service limits. 
+
+B. Security, Fault tolerance, high availability, connectivity, service limits 
+
+C. Performance, cost optimization, access control, connectivity, security 
+
+D. Security, access control, high availability, performance, service limits
+
+Answer: [ A ] 
+
+                       Performance, cost optimization, security, fault tolerance, service limits
+
+Q6. What is the focus of the sustainability pillar of the well - architected Frame work ? 
+
+A. Designing workloads that recover quickly from failures. 
+
+B. Minimize the environmental impacts of the running cloud networks. 
+
+C. Avoid unnecessary cost in cloud networks. 
+
+D. Automating updates to cloud networks. 
+
+Answer: [ B ] Minimize the environmental impacts of the running cloud networks. 
+
+Q7. The AWS Well-Architected Framework is organized into how many pillars?
+
+Answer:  5
+
+Q8. After you move to the AWS Cloud, you want to ensure that the right security settings are put 
+
+       in place. Which online tool can assist in security compliance?
+
+A. Amazon Kinesis 
+
+B. Amazon Support 
+
+C. AWS Trusted Advisor 
+
+D. Amazon Cloud watch 
+
+Answer: [ C]  AWS Trusted Advisor
+
+Q9. Which of the following is a measure of your system's ability to provide functionality when 
+
+
+       desired by the user?
+
+A. Availability 
+
+B. Fault Tolerance 
+
+C. Reliability 
+
+D. Performance Efficiency 
+
+
+Answer: [ C]  Reliability
+
+Q10.  What is defined as the ability for a system to remain operational even if some of the 
+
+           components of that system fail?
+
+A. High durability 
+
+
+B. Fault Tolerance 
+
+C. High availability 
+
+Answer: [ B ] Fault Tolerance
+
+Q11. Which of the following best describes a system that can withstand some measures of 
+
+        degradation, experiences minimal downtime, and requires minimal human intervention
+
+A. Scalable 
+
+B. Fault Tolerant 
+
+C. Elastic 
+
+D. Highly available 
+
+Answer: [ D ] Highly available
